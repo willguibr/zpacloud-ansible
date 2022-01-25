52d9910ab7687c231623e0a8aee96c460f312744
amazzalel-habib <amazzal.elhabib@gmail.com>
2022-01-25 14:14:11 +0100

    Add lss config resource & data

--- plugins/module_utils/zpa_lss_config_controller.py
+++ plugins/module_utils/zpa_lss_config_controller.py
@@ -1,6 +1,6 @@
 import re
 from ansible_collections.willguibr.zpacloud.plugins.module_utils.zpa_client import (
-    ZPAClientHelper,
+    ZPAClientHelper, delete_none, camelcaseToSnakeCase, snakecaseToCamelcase
 )
 
 
@@ -11,12 +11,12 @@ class LSSConfigControllerService:
         self.rest = ZPAClientHelper(module)
 
     def getByIDOrName(self, id, name):
-        app = None
+        lss_config = None
         if id is not None:
-            app = self.getByID(id)
-        if app is None and name is not None:
-            app = self.getByName(name)
-        return app
+            lss_config = self.getByID(id)
+        if lss_config is None and name is not None:
+            lss_config = self.getByName(name)
+        return lss_config
 
     def getByID(self, id):
         response = self.rest.get(
@@ -29,67 +29,38 @@ class LSSConfigControllerService:
     def getAll(self):
         list = self.rest.get_paginated_data(
             base_url="/mgmtconfig/v2/admin/customers/%s/lssConfig" % (self.customer_id), data_key_name="list")
-        apps = []
-        for app in list:
-            apps.append(self.mapRespJSONToApp(app))
-        return apps
+        lss_configs = []
+        for lss_config in list:
+            lss_configs.append(self.mapRespJSONToApp(lss_config))
+        return lss_configs
 
     def getByName(self, name):
-        apps = self.getAll()
-        for app in apps:
-            if app.get("name") == name:
-                return app
+        lss_configs = self.getAll()
+        for lss_config in lss_configs:
+            if lss_config.get("config", {}).get("name") == name:
+                return lss_config
         return None
-
-    def mapConnectorsJSONToList(self, connectors):
-        if connectors is None:
-            return []
-        l = []
-        for s in connectors:
-            d = self.camelcaseToSnakeCase(s)
-            l.append(d)
-        return l
-
-    @staticmethod
-    def camelcaseToSnakeCase(obj):
-        new_obj = dict()
-        for key, value in obj.items():
-            if value is not None:
-                new_obj[re.sub(r'(?<!^)(?=[A-Z])', '_', key).lower()] = value
-        return new_obj
 
     def mapListJSONToList(self, entities):
         if entities is None:
             return []
         l = []
         for s in entities:
-            l.append(self.camelcaseToSnakeCase(s))
+            l.append(camelcaseToSnakeCase(s))
         return l
 
+    def mapListToJSONList(self, entities):
+        if entities is None:
+            return []
+        l = []
+        for e in entities:
+            l.append(dict(id=e.get("id")))
+        return l
 
-    def mapConfigToListJSON(self, configJSON):
-        configs = []
-        for config in configJSON:
-            configs.append({
-                "auditMessage": config.get("audit_message"),
-                "creationTime": config.get("creation_time"),
-                "description": config.get("description"),
-                "enabled": config.get("enabled"),
-                "filter": config.get("filter"),
-                "format": config.get("format"),
-                "id": config.get("id"),
-                "modifiedBy": config.get("modified_by"),
-                "modifiedTime": config.get("modified_time"),
-                "name": config.get("name"),
-                "lssHost": config.get("lss_host"),
-                "lssPort": config.get("lss_port"),
-                "sourceLogType": config.get("source_log_type"),
-                "useTls": config.get("use_tls"),
-            })
-        return configs
-    
     def mapOperandsToList(self, operandsJSON):
         ops = []
+        if operandsJSON is None:
+            return None
         for op in operandsJSON:
             ops.append({
                 "id": op.get("id"),
@@ -101,9 +72,11 @@ class LSSConfigControllerService:
                 "name": op.get("name"),
             })
         return ops
-    
+
     def mapOperandsToListJSON(self, operandsJSON):
         ops = []
+        if operandsJSON is None:
+            return None
         for op in operandsJSON:
             ops.append({
                 "objectType": op.get("object_type"),
@@ -112,9 +85,11 @@ class LSSConfigControllerService:
                 "name": op.get("name"),
             })
         return ops
-    
+
     def mapConditionsToList(self, conditionsJSON):
         conds = []
+        if conditionsJSON is None:
+            return None
         for cond in conditionsJSON:
             """"""
             conds.append({
@@ -130,6 +105,8 @@ class LSSConfigControllerService:
 
     def mapConditionsToJSONList(self, conditions):
         conds = []
+        if conditions is None:
+            return None
         for cond in conditions:
             """"""
             conds.append({
@@ -138,30 +115,32 @@ class LSSConfigControllerService:
                 "operands": self.mapOperandsToListJSON(cond.get("operands")),
             })
         return conds
-    
+
+    @delete_none
     def mapRespJSONToApp(self, resp_json):
         if resp_json is None:
             return {}
         return {
             "id": resp_json.get("id"),
-            "config": self.mapConfigToListJSON(resp_json.get("config")),
+            "config": camelcaseToSnakeCase(resp_json.get("config")),
             "connector_groups": self.mapListJSONToList(resp_json.get("connectorGroups")),
             "conditions": self.mapConditionsToList(resp_json.get("conditions")),
         }
-    
+
+    @delete_none
     def mapAppToJSON(self, policy_rule):
         if policy_rule is None:
             return {}
-        return self.delete_none({
+        return {
             "id": policy_rule.get("id"),
-            "config": policy_rule.get("config"),
-            "connectorGroups": policy_rule.get("connector_groups"),
+            "config": snakecaseToCamelcase(policy_rule.get("config")),
+            "connectorGroups": self.mapListToJSONList(policy_rule.get("connector_groups")),
             "conditions": self.mapConditionsToJSONList(policy_rule.get("conditions")),
-        })
-    
-    def create(self, app):
+        }
+
+    def create(self, lss_config):
         """Create new LSSConfig"""
-        appJSON = self.mapAppToJSON(app)
+        appJSON = self.mapAppToJSON(lss_config)
         response = self.rest.post(
             "/mgmtconfig/v2/admin/customers/%s/lssConfig" % (self.customer_id), data=appJSON)
         status_code = response.status_code
@@ -169,15 +148,15 @@ class LSSConfigControllerService:
             return None
         return self.mapRespJSONToApp(response.json)
 
-    def update(self, app):
+    def update(self, lss_config):
         """update the LSSConfig"""
-        appJSON = self.mapAppToJSON(app)
+        appJSON = self.mapAppToJSON(lss_config)
         response = self.rest.put(
             "/mgmtconfig/v2/admin/customers/%s/lssConfig/%s" % (self.customer_id, appJSON.get("id")), data=appJSON)
         status_code = response.status_code
         if status_code > 299:
             return None
-        return app
+        return lss_config
 
     def delete(self, id):
         """delete the LSSConfig"""
--- plugins/modules/zpa_lss_config_controller.py
+++ plugins/modules/zpa_lss_config_controller.py
@@ -15,50 +15,129 @@ __metaclass__ = type
 
 DOCUMENTATION = """
 ---
+module: zpa_lss_config_controller
+short_description: Create a LSS CONFIG.
+description:
+  - This module create/update/delete a LSS CONFIG in the ZPA Cloud.
+author:
+  - William Guilherme (@willguibr)
+version_added: "1.0.0"
+options:
+  id:
+    type: str
+    description: ""
+  policy_rule_resource:
+    type: list
+    elements: dict
+    required: False
+    description: ""
+  connector_groups:
+    type: list
+    elements: dict
+    required: False
+    description:  "App Connector Group(s) to be added to the LSS configuration"
+  config:
+    type: list
+    elements: dict
+    required: False
+    description: ""
+"""
+EXAMPLES = '''
+- name: LSS Controller
+  hosts: localhost
+  tasks:
+    - name: Create a LSS Controller
+      willguibr.zpacloud.zpa_lss_config_controller:
+        state: present
+        config:
+          name: Status
+          description: status
+          enabled: true
+          lss_host: 10.1.1.1
+          lss_port: 20000
+          format: {\"LogTimestamp\": %j{LogTimestamp:time},\"Customer\": %j{Customer},\"SessionID\": %j{SessionID},\"SessionType\": %j{SessionType},\"SessionStatus\": %j{SessionStatus},\"Version\": %j{Version},\"Platform\": %j{Platform},\"ZEN\": %j{ZEN},\"Connector\": %j{Connector},\"ConnectorGroup\": %j{ConnectorGroup},\"PrivateIP\": %j{PrivateIP},\"PublicIP\": %j{PublicIP},\"Latitude\": %f{Latitude},\"Longitude\": %f{Longitude},\"CountryCode\": %j{CountryCode},\"TimestampAuthentication\": %j{TimestampAuthentication:iso8601},\"TimestampUnAuthentication\": %j{TimestampUnAuthentication:iso8601},\"CPUUtilization\": %d{CPUUtilization},\"MemUtilization\": %d{MemUtilization},\"ServiceCount\": %d{ServiceCount},\"InterfaceDefRoute\": %j{InterfaceDefRoute},\"DefRouteGW\": %j{DefRouteGW},\"PrimaryDNSResolver\": %j{PrimaryDNSResolver},\"HostUpTime\": %j{HostUpTime},\"ConnectorUpTime\": %j{ConnectorUpTime},\"NumOfInterfaces\": %d{NumOfInterfaces},\"BytesRxInterface\": %d{BytesRxInterface},\"PacketsRxInterface\": %d{PacketsRxInterface},\"ErrorsRxInterface\": %d{ErrorsRxInterface},\"DiscardsRxInterface\": %d{DiscardsRxInterface},\"BytesTxInterface\": %d{BytesTxInterface},\"PacketsTxInterface\": %d{PacketsTxInterface},\"ErrorsTxInterface\": %d{ErrorsTxInterface},\"DiscardsTxInterface\": %d{DiscardsTxInterface},\"TotalBytesRx\": %d{TotalBytesRx},\"TotalBytesTx\": %d{TotalBytesTx}}\\n"
+          source_log_type: "zpn_ast_auth_log"
+        connector_groups:
+          - id: "11111"
+            name: "Test"
+      register: lss_controller
+    - name: lss_controller
+      debug:
+        msg: "{{ lss_controller }}"
+'''
+
+RETURN = """
+# The newly created browser access application segment resource record.
+{
+            "config": {
+                "audit_message": "{\"logType\":\"App Connector Status\",\"tcpPort\":\"20000\",\"appConnectorGroups\":[{\"name\":null,\"id\":\"216196257331292512\"}],\"domainOrIpAddress\":\"10.1.1.1\",\"logStreamContent\":\"{\\\"LogTimestamp\\\": %j{LogTimestamp:time},\\\"Customer\\\": %j{Customer},\\\"SessionID\\\": %j{SessionID},\\\"SessionType\\\": %j{SessionType},\\\"SessionStatus\\\": %j{SessionStatus},\\\"Version\\\": %j{Version},\\\"Platform\\\": %j{Platform},\\\"ZEN\\\": %j{ZEN},\\\"Connector\\\": %j{Connector},\\\"ConnectorGroup\\\": %j{ConnectorGroup},\\\"PrivateIP\\\": %j{PrivateIP},\\\"PublicIP\\\": %j{PublicIP},\\\"Latitude\\\": %f{Latitude},\\\"Longitude\\\": %f{Longitude},\\\"CountryCode\\\": %j{CountryCode},\\\"TimestampAuthentication\\\": %j{TimestampAuthentication:iso8601},\\\"TimestampUnAuthentication\\\": %j{TimestampUnAuthentication:iso8601},\\\"CPUUtilization\\\": %d{CPUUtilization},\\\"MemUtilization\\\": %d{MemUtilization},\\\"ServiceCount\\\": %d{ServiceCount},\\\"InterfaceDefRoute\\\": %j{InterfaceDefRoute},\\\"DefRouteGW\\\": %j{DefRouteGW},\\\"PrimaryDNSResolver\\\": %j{PrimaryDNSResolver},\\\"HostUpTime\\\": %j{HostUpTime},\\\"ConnectorUpTime\\\": %j{ConnectorUpTime},\\\"NumOfInterfaces\\\": %d{NumOfInterfaces},\\\"BytesRxInterface\\\": %d{BytesRxInterface},\\\"PacketsRxInterface\\\": %d{PacketsRxInterface},\\\"ErrorsRxInterface\\\": %d{ErrorsRxInterface},\\\"DiscardsRxInterface\\\": %d{DiscardsRxInterface},\\\"BytesTxInterface\\\": %d{BytesTxInterface},\\\"PacketsTxInterface\\\": %d{PacketsTxInterface},\\\"ErrorsTxInterface\\\": %d{ErrorsTxInterface},\\\"DiscardsTxInterface\\\": %d{DiscardsTxInterface},\\\"TotalBytesRx\\\": %d{TotalBytesRx},\\\"TotalBytesTx\\\": %d{TotalBytesTx}}\\\\n\",\"name\":\"Status\",\"description\":\"status\",\"sessionStatuses\":null,\"enabled\":true,\"useTls\":false,\"policy\":{}}",
+                "creation_time": "1643109694",
+                "description": "status",
+                "enabled": true,
+                "format": "{\"LogTimestamp\": %j{LogTimestamp:time},\"Customer\": %j{Customer},\"SessionID\": %j{SessionID},\"SessionType\": %j{SessionType},\"SessionStatus\": %j{SessionStatus},\"Version\": %j{Version},\"Platform\": %j{Platform},\"ZEN\": %j{ZEN},\"Connector\": %j{Connector},\"ConnectorGroup\": %j{ConnectorGroup},\"PrivateIP\": %j{PrivateIP},\"PublicIP\": %j{PublicIP},\"Latitude\": %f{Latitude},\"Longitude\": %f{Longitude},\"CountryCode\": %j{CountryCode},\"TimestampAuthentication\": %j{TimestampAuthentication:iso8601},\"TimestampUnAuthentication\": %j{TimestampUnAuthentication:iso8601},\"CPUUtilization\": %d{CPUUtilization},\"MemUtilization\": %d{MemUtilization},\"ServiceCount\": %d{ServiceCount},\"InterfaceDefRoute\": %j{InterfaceDefRoute},\"DefRouteGW\": %j{DefRouteGW},\"PrimaryDNSResolver\": %j{PrimaryDNSResolver},\"HostUpTime\": %j{HostUpTime},\"ConnectorUpTime\": %j{ConnectorUpTime},\"NumOfInterfaces\": %d{NumOfInterfaces},\"BytesRxInterface\": %d{BytesRxInterface},\"PacketsRxInterface\": %d{PacketsRxInterface},\"ErrorsRxInterface\": %d{ErrorsRxInterface},\"DiscardsRxInterface\": %d{DiscardsRxInterface},\"BytesTxInterface\": %d{BytesTxInterface},\"PacketsTxInterface\": %d{PacketsTxInterface},\"ErrorsTxInterface\": %d{ErrorsTxInterface},\"DiscardsTxInterface\": %d{DiscardsTxInterface},\"TotalBytesRx\": %d{TotalBytesRx},\"TotalBytesTx\": %d{TotalBytesTx}}\\n",
+                "id": "216196257331292567",
+                "lss_host": "10.1.1.1",
+                "lss_port": "20000",
+                "modified_by": "216196257331282070",
+                "name": "Status",
+                "source_log_type": "zpn_ast_auth_log",
+                "use_tls": false
+            },
+            "connector_groups": [
+                {
+                    "city_country": "Langley, CA",
+                    "country_code": "CA",
+                    "creation_time": "1642798564",
+                    "description": "USA App Connector Group",
+                    "dns_query_type": "IPV4",
+                    "enabled": true,
+                    "id": "216196257331292512",
+                    "latitude": "49.1041779",
+                    "location": "Langley City, BC, Canada",
+                    "longitude": "-122.6603519",
+                    "lss_app_connector_group": false,
+                    "modified_by": "216196257331282070",
+                    "name": "USA App Connector Group",
+                    "override_version_profile": true,
+                    "upgrade_day": "SUNDAY",
+                    "upgrade_time_in_secs": "66600",
+                    "version_profile_id": "2",
+                    "version_profile_name": "New Release",
+                    "version_profile_visibility_scope": "ALL"
+                }
+            ],
+            "id": "216196257331292567"
+        }
 """
 
 def core(module):
     state = module.params.get("state", None)
     customer_id = module.params.get("customer_id", None)
     service = LSSConfigControllerService(module, customer_id)
-    app = dict()
+    lss_config = dict()
     params = [
         "id",
         "config",
         "connector_groups",
         "policy_rule_resource"
-        "enabled",
-        "city_country",
-        "country_code",
-        "latitude",
-        "longitude",
-        "location",
-        "upgrade_day",
-        "upgrade_time_in_secs",
-        "override_version_profile",
-        "version_profile_id",
-        "dns_query_type",
     ]
     for param_name in params:
-        app[param_name] = module.params.get(param_name, None)
-    existing_app = service.getByIDOrName(app.get("id"), app.get("name"))
-    if existing_app is not None:
-        id = existing_app.get("id")
-        existing_app.update(app)
-        existing_app["id"] = id
+        lss_config[param_name] = module.params.get(param_name, None)
+    existing_lss_config = service.getByIDOrName(
+        lss_config.get("id"), lss_config.get("config", {}).get("name"))
     if state == "present":
-        if existing_app is not None:
+        if existing_lss_config is not None:
             """Update"""
-            service.update(existing_app)
-            module.exit_json(changed=True, data=existing_app)
+            existing_lss_config = service.update(existing_lss_config)
+            module.exit_json(changed=True, data=existing_lss_config)
         else:
             """Create"""
-            app = service.create(app)
-            module.exit_json(changed=False, data=app)
+            lss_config = service.create(lss_config)
+            module.exit_json(changed=False, data=lss_config)
     elif state == "absent":
-        if existing_app is not None:
-            service.delete(existing_app.get("id"))
-            module.exit_json(changed=False, data=existing_app)
+        if existing_lss_config is not None:
+            service.delete(existing_lss_config.get("id"))
+            module.exit_json(changed=False, data=existing_lss_config)
     module.exit_json(changed=False, data={})
 
 
@@ -68,54 +147,91 @@ def main():
     id_name_spec = dict(type='list', elements='dict', options=dict(id=dict(
         type='str', required=True), name=dict(type='str', required=False)), required=False)
     argument_spec.update(
-        config=dict(
-            type='dict',
-            options=dict(
-                audit_message=dict(type="str", required=False),
-                creation_time=dict(type="str", required=False),
-                description=dict(type="str", required=False),
-                enabled=dict(type="bool", default=True, required=False),
-                filter=dict(type="str", required=False),
-                format=dict(type="str", required=False),
-                id=dict(type="str", required=False),
-                modified_by=dict(type="str", required=False),
-                modified_time=dict(type="str", required=False),
-                name=dict(type="str", required=True),
-                lss_host=dict(type="str", required=True),
-                lss_port=dict(type="str", required=True),
-                source_log_type=dict(choices=['zpn_trans_log', 'zpn_auth_log', 'zpn_ast_auth_log', 'zpn_http_trans_log', 'zpn_audit_log', 'zpn_sys_auth_log', 'zpn_http_insp', 'zpn_ast_comprehensive_stats']),
-                use_tls=dict(type="bool", required=False),
-                ),
-            ),
+        id=dict(type='str'),
         policy_rule_resource=dict(
             type='dict',
             options=dict(
-                action=dict(type='str', required=False, choices=["ALLOW", "DENY"]),
-                #policy_set_id=dict(type='str', required=True),
-                id=dict(type='str'),
-                conditions=dict(type='list', elements='dict', options=dict(id=dict(type='str'),
-                                                                   negated=dict(
-                                                                       type='bool', required=False),
-                                                                   operator=dict(
-                                                                       type='str', required=False),
-                                                                   operands=dict(type='list', elements='dict', options=dict(id=dict(type='str'),
-                                                                                                                            idp_id=dict(
-                                                                                                                                type='str', required=False),
-                                                                                                                            name=dict(
-                                                                                                                                type='str', required=False),
-                                                                                                                            lhs=dict(
-                                                                                                                                type='str', required=True),
-                                                                                                                            rhs=dict(
-                                                                                                                                type='str', required=False),
-                                                                                                                            rhs_list=dict(
-                                                                        type='list', elements='str', required=False),
-                                                                        object_type=dict(
-                                                                        type='str', required=False, choices=["APP", "APP_GROUP", "CLIENT_TYPE"]),
-                                                                   ), required=False),
-                                                                   ), required=False),
+                priority=dict(
+                    type='str', required=False
                 ),
+                reauth_idle_timeout=dict(
+                    type='str', required=False
+                ),
+                policy_type=dict(
+                    type='str', required=False
+                ),
+                reauth_default_rule=dict(
+                    type='bool', required=False
+                ),
+                custom_msg=dict(
+                    type='str', required=False
+                ),
+                action_id=dict(
+                    type='str', required=False
+                ),
+                operator=dict(
+                    type='str', required=False
+                ),
+                bypass_default_rule=dict(
+                    type='bool', required=False
+                ),
+                policy_set_id=dict(
+                    type='str', required=False
+                ),
+                default_rule=dict(
+                    type='bool', required=False
+                ),
+                action=dict(
+                    type='str', required=False
+                ),
+                conditions=dict(
+                    type='list',
+                    elements='dict',
+                    options=dict(
+                        negated=dict(type='bool', required=False),
+                        operator=dict(type='str', required=True),
+                        operands=dict(
+                            type='list',
+                            elements='dict',
+                            options=dict(
+                                values=dict(
+                                    type='list', elements='str', required=False
+                                ),
+                                object_type=dict(
+                                    type='str', required=True, choices=["APP", "APP_GROUP", "CLIENT_TYPE"]
+                                ),
+                            ),
+                            required=False
+                        ),
+                    ), required=False),
+                name=dict(type='str', required=True),
+                reauth_timeout=dict(type='str', required=False),
+                rule_order=dict(type='str', required=False),
+                description=dict(type='str', required=False),
+                id=dict(type='str'),
+                lss_default_rule=dict(type='bool', required=False),
             ),
-        app_connector_groups=id_name_spec,
+            required=False
+        ),
+        connector_groups=id_name_spec,
+        config=dict(
+            type='dict',
+            options=dict(
+                format=dict(type='str', required=True),
+                id=dict(type='str'),
+                name=dict(type='str', required=True),
+                lss_port=dict(type='str', required=True),
+                use_tls=dict(type='bool', required=False, default=False),
+                enabled=dict(type='bool', required=False, default=True),
+                description=dict(type='str', required=False),
+                filter=dict(type='list', elements='str', required=False),
+                lss_host=dict(type='str', required=True),
+                source_log_type=dict(type='str', required=True, choices=["zpn_trans_log", "zpn_auth_log", "zpn_ast_auth_log",
+                                     "zpn_http_trans_log", "zpn_audit_log", "zpn_sys_auth_log", "zpn_http_insp", "zpn_ast_comprehensive_stats", ]),
+                audit_message=dict(type='str', required=False),
+            ),
+            required=False
+        ),
         state=dict(type="str", choices=[
                    "present", "absent"], default="present"),
     )
